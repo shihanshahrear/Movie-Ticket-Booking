5fa8330e8c4b1ce090e1a3235f8cb7cb5b705eb0
Shahrear Hossain Shihan <134633118+shihanshahrear@users.noreply.github.com>
2025-12-03 11:29:50 +0600

    Add file

---
+++ admin-home.js
@@ -0,0 +1,7 @@
+const addBtn = document.getElementById("addMovieBtn");
+
+if (addBtn) {
+    addBtn.addEventListener("click", function () {
+        alert("Demo: ekhane future e movie add form banate parba.");
+    });
+}
