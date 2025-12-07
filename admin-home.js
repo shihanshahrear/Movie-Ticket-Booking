3fba067fbf27a6a7bd8e30e1a3c5ae49eeeb63e0
Shahrear Hossain Shihan <134633118+shihanshahrear@users.noreply.github.com>
2025-12-07 19:02:33 +0600

    Delete admin-home.js

--- admin-home.js
+++
@@ -1,7 +0,0 @@
-const addBtn = document.getElementById("addMovieBtn");
-
-if (addBtn) {
-    addBtn.addEventListener("click", function () {
-        alert("Demo: ekhane future e movie add form banate parba.");
-    });
-}
